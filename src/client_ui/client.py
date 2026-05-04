9a9d834a3ae24206c645d90fd9cbfc39ccb4d7c7
Robin Walz <robin.walz.wien@gmail.com>
2026-05-04 12:10:56 +0200

    update UI client to work with venv imports

--- src/client_ui/client.py
+++ src/client_ui/client.py
@@ -5,6 +5,7 @@ import socket
 import os
 import threading
 import customtkinter
+from customtkinter import *
 import tkinter
 from tkinter import *
 from PIL import Image
@@ -274,13 +275,15 @@ def print_msg(msg):
     global frame_canvas
   
 
-    new_label = customtkinter.CTkLabel(master=frame_labels,
-                                                text=msg,
-                                                font=(FONT, 15),
-                                                height=40,
-                                                corner_radius=7,
-                                                fg_color=(DARK_BG_GREY),
-                                                anchor="w")
+    new_label = CTkLabel(
+        master=frame_labels,
+        text=msg,
+        font=(FONT, 15),
+        height=40,
+        corner_radius=7,
+        fg_color=(DARK_BG_GREY),
+        anchor="w"
+    )
 
 
     new_label.pack(side=TOP, anchor=NW, padx=10, pady=5)
@@ -353,7 +356,7 @@ def newClientConnectedUI(user_id, row):
     username = client_list[user_id]
 
     #add a new Label 
-    newButton = customtkinter.CTkButton(frame_main, text=username, font=(FONT, 12), command=lambda: ContactButtonEvent(user_id), height=rel_Height(45), border_width=3, border_color=LIME_GREEN, fg_color=DARK_BG_GREY) 
+    newButton = CTkButton(frame_main, text=username, font=(FONT, 12), command=lambda: ContactButtonEvent(user_id), height=rel_Height(45), border_width=3, border_color=LIME_GREEN, fg_color=DARK_BG_GREY) 
     newButton.grid(row=_row, column=0, sticky="nesw",padx=rel_Width(20) ,pady=rel_Height(8))    
     client_buttons_list.append(newButton)
 
@@ -484,8 +487,8 @@ def init_UI():
     window.state('zoomed')
 
     #create the frames
-    frame_main = customtkinter.CTkFrame(window)
-    frame_login = customtkinter.CTkFrame(window)
+    frame_main = CTkFrame(window)
+    frame_login = CTkFrame(window)
     for frame in (frame_main, frame_login):
         frame.grid(row=0,column=0,sticky='nsew')
 
@@ -500,43 +503,43 @@ def init_UI():
     frame_login.grid_columnconfigure(2, weight=1)
 
 
-    chat_label = customtkinter.CTkLabel(frame_main, text="Settings/Chat")
+    chat_label = CTkLabel(frame_main, text="Settings/Chat")
     chat_label.configure(font=(FONT, 15))
     chat_label.grid(row=0, column=0, padx=(20, 80))
 
-    _Checkbox = customtkinter.CTkCheckBox(master=frame_main, text="", command=toggleFullscreen)
+    _Checkbox = CTkCheckBox(master=frame_main, text="", command=toggleFullscreen)
     _Checkbox.grid(row=0, column=1, sticky="nw", pady=30)
 
-    chat_label = customtkinter.CTkLabel(frame_main, text="Chat:", text_color="white")
+    chat_label = CTkLabel(frame_main, text="Chat:", text_color="white")
     chat_label.configure(font=(FONT, 15))
     chat_label.grid(row=0, column=2)
 
 
     #connect to server button
-    server_button = customtkinter.CTkButton(frame_main, text="Connect to Server", command=startConnectingThread, corner_radius=5, font=(FONT, 15), width=80, height=50)
+    server_button = CTkButton(frame_main, text="Connect to Server", command=startConnectingThread, corner_radius=5, font=(FONT, 15), width=80, height=50)
     server_button.grid(row=1, column=0, sticky="ne", pady=30)
 
-    info_label = customtkinter.CTkLabel(frame_main, text="Logged in as:", text_color="white", font=(FONT, 15))
+    info_label = CTkLabel(frame_main, text="Logged in as:", text_color="white", font=(FONT, 15))
     info_label.grid(row=2, column=0)
 
-    username_label = customtkinter.CTkLabel(frame_main, text="[USERNAME]", text_color="white", font=(FONT, 15))
+    username_label = CTkLabel(frame_main, text="[USERNAME]", text_color="white", font=(FONT, 15))
     username_label.grid(row=3, column=0, pady=(0, 20))
 
     #message entry box
-    input_area = customtkinter.CTkEntry(frame_main, border_width=0, font=(FONT, 13))
+    input_area = CTkEntry(frame_main, border_width=0, font=(FONT, 13))
     input_area.grid(row=40, column=2, sticky="nsew", pady=10)
 
     #send button
-    send_button = customtkinter.CTkButton(frame_main, text="send", command=send_button_, corner_radius=5, font=(FONT, 12), width=rel_Width(80), height=rel_Height(50))
+    send_button = CTkButton(frame_main, text="send", command=send_button_, corner_radius=5, font=(FONT, 12), width=rel_Width(80), height=rel_Height(50))
     send_button.grid(row=40, column=3, columnspan=2, pady=10, padx=(0, 10))
 
    
     #light and dark mode
-    optionmenu_1 = customtkinter.CTkOptionMenu(master=frame_main, values=["Dark", "Light", "System", "Pink"], command=change_appearance_mode)
+    optionmenu_1 = CTkOptionMenu(master=frame_main, values=["Dark", "Light", "System", "Pink"], command=change_appearance_mode)
     optionmenu_1.grid(row=40, column=0, pady=10, padx=20, sticky="nesw")
 
     #Create a frame for the canvas with non-zero row&column weights
-    frame_canvas = customtkinter.CTkFrame(frame_main, border_width=0)
+    frame_canvas = CTkFrame(frame_main, border_width=0)
     frame_canvas.grid(row=1, column=2, columnspan=2, rowspan=35, sticky="nesw")
     frame_canvas.grid_rowconfigure(0, weight=1)
     frame_canvas.grid_columnconfigure(0, weight=1)
@@ -547,12 +550,12 @@ def init_UI():
     canvas.grid(row=0, column=0, sticky="news", columnspan=30)
 
     # Link a scrollbar to the canvas
-    vsb = customtkinter.CTkScrollbar(frame_main, command=canvas.yview)
+    vsb = CTkScrollbar(frame_main, command=canvas.yview)
     vsb.grid(row=1, column=4, rowspan=35, sticky="ns", padx=(0, 10))
     canvas.configure(yscrollcommand=vsb.set)
 
     # Create a frame to contain the buttons
-    frame_labels = customtkinter.CTkFrame(canvas, fg_color=LIGHT_BG_GREY)
+    frame_labels = CTkFrame(canvas, fg_color=LIGHT_BG_GREY)
     resize_frame = canvas.create_window((0, 0), window=frame_labels, anchor='nw',width=frame_canvas.winfo_width(), height=frame_canvas.winfo_height())
 
     # Update buttons frames idle tasks to let tkinter calculate buttons sizes
@@ -594,8 +597,7 @@ def init_UI():
     ip_label.grid(row=3, column=1, pady=(20, 10), sticky="s")
 
     global ip_area
-    ip_area = CTkEntry(frame_login, border_width=2, height=rel_Height(70), width=rel_Width(280), font=(FONT, 14), 
-                       placeholder_text="e.g., 127.0.0.1")
+    ip_area = CTkEntry(frame_login, border_width=2, height=rel_Height(70), width=rel_Width(280), font=(FONT, 14), placeholder_text="e.g., 127.0.0.1")
     ip_area.insert(0, "127.0.0.1")
     ip_area.grid(row=4, column=1, sticky="n", pady=10, padx=(0, 5))
 
@@ -604,8 +606,7 @@ def init_UI():
     username_label_login.configure(font=(FONT, 16))
     username_label_login.grid(row=3, column=3, pady=(20, 10), sticky="s")
 
-    username_area = CTkEntry(frame_login, border_width=2, height=rel_Height(70), width=rel_Width(280), font=(FONT, 14),
-                             placeholder_text="Enter your name")
+    username_area = CTkEntry(frame_login, border_width=2, height=rel_Height(70), width=rel_Width(280), font=(FONT, 14), placeholder_text="Enter your name")
     username_area.grid(row=4, column=3, sticky="n", pady=10, padx=(5, 0))
 
     # Login button (centered below both fields)
